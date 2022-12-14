4da1c4579ba09c16b6acda475fd88526e3635d89
Thunder Power Studios <107780353+Thunder-Power-Studios@users.noreply.github.com>
2022-12-13 19:30:49 -0500

    Add files via upload

---
+++ JS/New Years.js
@@ -0,0 +1,16 @@
+var countDownDate=new Date("Jan 1, 2023 0:0:0").getTime();
+var x=setInterval(function(){
+var now=new Date().getTime();
+var distance=countDownDate-now;
+var days=Math.floor(distance/(1000*60*60*24));
+var hours=Math.floor((distance%(1000*60*60*24))/(1000*60*60));
+var minutes=Math.floor((distance%(1000*60*60))/(1000*60));
+var seconds=Math.floor((distance%(1000*60))/1000);
+
+document.getElementById("newyears").innerHTML=days+"d "+hours+"h "+minutes+"m "+seconds+"s ";
+
+if(distance<0){
+clearInterval(x);
+document.getElementById("newyears").innerHTML="Happy New Year's!";
+}
+},1000);
