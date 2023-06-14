ddc8bcfc142e6cd11b2824173ca9f0d3840e71a0
Thunder Power Studios <107780353+Thunder-Power-Studios@users.noreply.github.com>
2023-06-14 10:29:25 -0400

    update

--- JS/Thanksgiving.js
+++ JS/Thanksgiving.js
@@ -1,4 +1,4 @@
-var countDownDate=new Date("May 27, 2024 0:0:0").getTime();
+var countDownDate=new Date("Nov 23, 2023 0:0:0").getTime();
 var x=setInterval(function(){
 var now=new Date().getTime();
 var distance=countDownDate-now;
@@ -7,10 +7,10 @@ var hours=Math.floor((distance%(1000*60*60*24))/(1000*60*60));
 var minutes=Math.floor((distance%(1000*60*60))/(1000*60));
 var seconds=Math.floor((distance%(1000*60))/1000);
 
-document.getElementById("MemorialDay").innerHTML=days+"d "+hours+"h "+minutes+"m "+seconds+"s ";
+document.getElementById("Thanksgiving").innerHTML=days+"d "+hours+"h "+minutes+"m "+seconds+"s ";
 
 if(distance<0){
 clearInterval(x);
-document.getElementById("MemorialDay").innerHTML="Happy Memorial Day!";
+document.getElementById("Thanksgiving").innerHTML="Happy Thanksgiving!";
 }
 },1000);
