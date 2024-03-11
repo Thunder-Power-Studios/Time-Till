b58bbd2ae6cecfc7cf7c97e43fe54f22435ee3fa
Thunder Power Studios <107780353+Thunder-Power-Studios@users.noreply.github.com>
2024-03-10 22:28:29 -0400

    month/date fix

--- JS/St. Patrick's Day.js
+++ JS/St. Patrick's Day.js
@@ -1,5 +1,5 @@
 // Same date every year
-var countDownDate=new Date("March 17, 2024 0:0:0").getTime();
+var countDownDate=new Date("Mar 17, 2024 0:0:0").getTime();
 var x=setInterval(function(){
 var now=new Date().getTime();
 var distance=countDownDate-now;
