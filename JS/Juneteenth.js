e5764ddb27f36330b4db28b52290fa255afce1b0
Thunder Power Studios <107780353+Thunder-Power-Studios@users.noreply.github.com>
2024-03-25 21:53:21 -0400

    Time Till Re-Branding Final

--- JS/Juneteenth.js
+++ JS/Juneteenth.js
@@ -1,5 +1,5 @@
 // Same date every year
-var countDownDate=new Date("Jun 19, 2024 0:0:0").getTime();
+var countDownDate=new Date("June 19, 2024 0:0:0").getTime();
 var x=setInterval(function(){
 var now=new Date().getTime();
 var distance=countDownDate-now;
--- JS/President's Day.js
+++ JS/President's Day.js
@@ -1,3 +1,4 @@
+// Same date every year
 var countDownDate=new Date("February 17, 2025 0:0:0").getTime();
 var x=setInterval(function(){
 var now=new Date().getTime();
