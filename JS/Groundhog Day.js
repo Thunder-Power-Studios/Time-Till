1f21c292564da7c9271a91094e175cb387a9f4c6
Thunder Power Studios <thunderpowerstudios@gmail.com>
2026-04-05 23:54:49 -0400

    Countdown + Added Events/Holidays

--- JS/Groundhog Day.js
+++ JS/Groundhog Day.js
@@ -1,5 +1,5 @@
-// Different date every year
-var countDownDate=new Date("March 28, 2027 0:0:0").getTime();
+// Same date every year
+var countDownDate=new Date("February 2, 2027 0:0:0").getTime();
 var x=setInterval(function(){
 var now=new Date().getTime();
 var distance=countDownDate-now;
@@ -8,10 +8,10 @@ var hours=Math.floor((distance%(1000*60*60*24))/(1000*60*60));
 var minutes=Math.floor((distance%(1000*60*60))/(1000*60));
 var seconds=Math.floor((distance%(1000*60))/1000);
 
-document.getElementById("Easter").innerHTML=days+"d "+hours+"h "+minutes+"m "+seconds+"s ";
+document.getElementById("GroundhogDay").innerHTML=days+"d "+hours+"h "+minutes+"m "+seconds+"s ";
 
 if(distance<0){
 clearInterval(x);
-document.getElementById("Easter").innerHTML="Happy Easter!";
+document.getElementById("GroundhogDay").innerHTML="Happy Groundhog Day! 6 more weeks of Winter or is Spring coming?";
 }
 },1000);
--- JS/Super Bowl Sunday.js
+++ JS/Super Bowl Sunday.js
@@ -1,5 +1,5 @@
 // Different date every year
-var countDownDate=new Date("March 28, 2027 0:0:0").getTime();
+var countDownDate=new Date("February 14, 2027 0:0:0").getTime();
 var x=setInterval(function(){
 var now=new Date().getTime();
 var distance=countDownDate-now;
@@ -8,10 +8,10 @@ var hours=Math.floor((distance%(1000*60*60*24))/(1000*60*60));
 var minutes=Math.floor((distance%(1000*60*60))/(1000*60));
 var seconds=Math.floor((distance%(1000*60))/1000);
 
-document.getElementById("Easter").innerHTML=days+"d "+hours+"h "+minutes+"m "+seconds+"s ";
+document.getElementById("SuperBowl").innerHTML=days+"d "+hours+"h "+minutes+"m "+seconds+"s ";
 
 if(distance<0){
 clearInterval(x);
-document.getElementById("Easter").innerHTML="Happy Easter!";
+document.getElementById("SuperBowl").innerHTML="Happy SuperBowl Sunday! Go Team!";
 }
 },1000);
