bc568caffaf5aa311f9a8c05dab01a084d8f7f60
Thunder Power Studios <thunderpowerstudios@gmail.com>
2026-07-22 19:00:40 -0400

    Countdown+ Update

---
+++ JS/Spiderman Brand New Day.js
@@ -0,0 +1,17 @@
+// Same/Different date every year
+var countDownDate=new Date("Jul 31, 2026 0:0:0").getTime();
+var x=setInterval(function(){
+var now=new Date().getTime();
+var distance=countDownDate-now;
+var days=Math.floor(distance/(1000*60*60*24));
+var hours=Math.floor((distance%(1000*60*60*24))/(1000*60*60));
+var minutes=Math.floor((distance%(1000*60*60))/(1000*60));
+var seconds=Math.floor((distance%(1000*60))/1000);
+
+document.getElementById("SMBND").innerHTML=days+"d "+hours+"h "+minutes+"m "+seconds+"s ";
+
+if(distance<0){
+clearInterval(x);
+document.getElementById("SMBND").innerHTML="Spiderman Brand New Day is out! ";
+}
+},1000);
---
+++ JS/US Elections.js
@@ -0,0 +1,17 @@
+// Same/Different date every year
+var countDownDate=new Date("Nov 3, 2026 0:0:0").getTime();
+var x=setInterval(function(){
+var now=new Date().getTime();
+var distance=countDownDate-now;
+var days=Math.floor(distance/(1000*60*60*24));
+var hours=Math.floor((distance%(1000*60*60*24))/(1000*60*60));
+var minutes=Math.floor((distance%(1000*60*60))/(1000*60));
+var seconds=Math.floor((distance%(1000*60))/1000);
+
+document.getElementById("USElection").innerHTML=days+"d "+hours+"h "+minutes+"m "+seconds+"s ";
+
+if(distance<0){
+clearInterval(x);
+document.getElementById("USElection").innerHTML="Happy Election Day! Go Vote!";
+}
+},1000);
